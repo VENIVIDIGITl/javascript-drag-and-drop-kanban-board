16f6b662017b034009c8d3256ab1897d6df74cf4
VeniVidiGiti <99083579+VENIVIDIGITl@users.noreply.github.com>
2022-04-18 19:29:54 +0300

    update DOM, data arrays and LocalStorage when dragging and dropping items from one column to another

--- script.js
+++ script.js
@@ -84,6 +84,31 @@ function updateDOM() {
   onHoldListArray.forEach((onHoldItem, index) => createItemEl(onHoldList, 0, onHoldItem, index));
 
   // Run getSavedColumns only once, Update Local Storage
+  updatedOnLoad = true;
+  updateSavedColumns();
+}
+
+
+// Allows arrays to reflect Drag and Drop items
+function rebuildArrays() {
+  backlogListArray = [];
+  for (let i = 0; i < backlogList.children.length; i++) {
+    backlogListArray.push(backlogList.children[i].textContent);
+  }
+  progressListArray = [];
+  for (let i = 0; i < progressList.children.length; i++) {
+    progressListArray.push(progressList.children[i].textContent);
+  }
+  completeListArray = [];
+  for (let i = 0; i < completeList.children.length; i++) {
+    completeListArray.push(completeList.children[i].textContent);
+  }
+  onHoldListArray = [];
+  for (let i = 0; i < onHoldList.children.length; i++) {
+    onHoldListArray.push(onHoldList.children[i].textContent);
+  }
+
+  updateDOM();
 }
 
 
@@ -114,6 +139,7 @@ function drop(event) {
   // Add Item to Column
   const parent = listColumns[currentColumn];
   parent.appendChild(draggedItem);
+  rebuildArrays();
 }
 
 
